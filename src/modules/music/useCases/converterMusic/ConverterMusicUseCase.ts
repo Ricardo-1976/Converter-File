59ed446d7cc33d3cdf534584ea746bbce820ebf1
Ricardo João António <90958374+Ricardo-1976@users.noreply.github.com>
2023-05-06 00:39:39 +0100

    set

---
+++ src/modules/music/useCases/converterMusic/ConverterMusicUseCase.ts
@@ -0,0 +1,11 @@
+
+interface IRequest {
+  file: string;
+}
+
+class ConverterMusicUseCase {
+  async execute({ file }: IRequest): Promise<void> {
+    
+  }
+}
+export { ConverterMusicUseCase };
--- src/modules/user/http/routes/user.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { CreateUserController } from '../../useCases/createUser/createUserController';
-
-const userRoutes = Router();
-
-
-export { userRoutes };
